149316896fae651cb34a57f2674eeba2d402015c
The MadDoG <105843577+teremuhamblin@users.noreply.github.com>
2026-03-29 11:33:23 +0200

    Add tests for hello() function

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,1 +1,8 @@
+const { hello } = require("@teremuhamblin/start.npm");
 
+describe("hello()", () => {
+  test("retourne 'Hello, World!'", () => {
+    const result = hello();
+    expect(result).toBe("Hello, World!");
+  });
+});
