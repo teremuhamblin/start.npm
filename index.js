231332bf3d37d2f80897f7389c53b3dfad266d9c
The MadDoG <105843577+teremuhamblin@users.noreply.github.com>
2026-03-29 14:17:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function add(a, b) {
-  return a + b;
+{
+  "name": "@teremuhamblin/start.npm",
+  "version": "1.0.0",
+  "description": "Un micro-starter pour initier rapidement un projet Node.js.",
+  "main": "index.js",
+  "scripts": {
+    "test": "jest"
+  },
+  "keywords": [
+    "starter",
+    "node",
+    "npm",
+    "template"
+  ],
+  "author": "Teremu",
+  "license": "MIT",
+  "devDependencies": {
+    "jest": "^29.7.0"
+  },
+  "publishConfig": {
+    "registry": "https://npm.pkg.github.com"
+  }
 }
-
-function greet(name) {
-  return `Bonjour, ${name} !`;
-}
-
-function hello() {
-  return "Hello World";
-}
-
-module.exports = {
-  add,
-  greet,
-  hello
-};
