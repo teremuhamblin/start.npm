dc03ee5319630110c4cf5cc8d595de38281430e0
The MadDoG <105843577+teremuhamblin@users.noreply.github.com>
2026-03-29 11:33:47 +0200

    Add tests for greet function

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,1 +1,13 @@
+const { greet } = require("@teremuhamblin/start.npm");
 
+describe("greet(name)", () => {
+  test("retourne un message personnalisé avec le nom fourni", () => {
+    const result = greet("Teremu");
+    expect(result).toBe("Hello, Teremu!");
+  });
+
+  test("gère un nom vide ou undefined", () => {
+    const result = greet();
+    expect(result).toBe("Hello, undefined!");
+  });
+});
