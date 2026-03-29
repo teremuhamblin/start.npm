3c94014e5ff82164ea214cdfe13a2f653e8e28ef
The MadDoG <105843577+teremuhamblin@users.noreply.github.com>
2026-03-29 09:46:24 +0200

    Create index.js with hello function export

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Simple module d'exemple pour GitHub Packages.
+ * Exporte une fonction qui retourne un message.
+ */
+
+function hello() {
+  return "Hello, World!";
+}
+
+module.exports = {
+  hello,
+};
